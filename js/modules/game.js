f9ae411bb056b59384495d521d86eb1e7b890fda
Evgeniy Osipov <eosipov446@gmail.com>
2026-04-23 18:33:36 +0500

    Update game.js

--- js/modules/game.js
+++ js/modules/game.js
@@ -1,4 +1,5 @@
-function fire(player, board) {
+function fire(player, board) 
+{
     const p = gameState[player];
     if (p.bullets.length > 2) return; 
     const b = { x: p.x + 14, y: p.y + 14, dir: p.deg, el: document.createElement('div') };
@@ -7,10 +8,12 @@ function fire(player, board) {
     p.bullets.push(b);
 }
 
-function resetPos() {
+function resetPos() 
+{
     gameState.p1.x = 40; gameState.p1.y = 40; gameState.p1.deg = 90;
     gameState.p2.x = 920; gameState.p2.y = 520; gameState.p2.deg = 270;
-    [gameState.p1, gameState.p2].forEach(p => { 
+    [gameState.p1, gameState.p2].forEach(p => 
+    { 
         p.bullets.forEach(b => b.el.remove()); 
         p.bullets = []; 
     });
